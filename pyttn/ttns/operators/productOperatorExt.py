a517c23740f48c4cc7d36f40cffe386c221a923e
Lachlan Lindoy <lplindoy@gmail.com>
2024-11-25 16:54:05 +0000

    Fixed an argument ordering issue with the python interface for the product_operator.

--- pyttn/ttns/operators/productOperatorExt.py
+++ pyttn/ttns/operators/productOperatorExt.py
@@ -9,8 +9,9 @@ except ImportError:
     ttn_real = None 
     SOP_real = None
 
-def product_operator(h, sysinf, dtype = np.complex128, *args, **kwargs):
+def product_operator(h, sysinf, *args, dtype = np.complex128, **kwargs):
     if(dtype == np.complex128 or product_operator_real is None):
+        print(len(args), len(kwargs))
         return product_operator_complex(h, sysinf, *args, **kwargs)
     else:
         return product_operator_real(h, sysinf, *args, **kwargs)
