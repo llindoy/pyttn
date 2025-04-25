ca5e750abc5a00db9582f750f76495cf130dfe64
Lachlan Lindoy <lachlan.lindoy@npl.co.uk>
2025-04-25 09:30:40 +0100

    Have begun writting the topology submodule implementing automated tree generation from some distance graph.  Extended spanning tree implementation to support a maximum degree and support degree 1 leading to construction of MPS style structure.

---
+++ pyttn/ttns/topology/bondDimensionSetter.py
@@ -0,0 +1,78 @@
+# This files is part of the pyTTN package.
+#(C) Copyright 2025 NPL Management Limited
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+import abc
+import numpy as np
+
+class BondDimensionSetter(metaclass=abc.ABCMeta):
+   """Abstract base class for objects that set the bond dimension variables
+   stored in an ntree object dependent on the values in the children of the node.
+   """
+   @abc.abstractmethod
+   def __call__(self, node):
+      """The default call function 
+
+      :param node: The node which will have its bond dimension set
+      :type node: ntreeNode
+      """
+      pass
+
+class NodeSumSetter(BondDimensionSetter):
+   """An implementation of the BondDimensionSetter abstract base class that set a given node
+   value based on the values of the nodes below the current node
+   """
+   def __call__(self, node):
+      """Set the value of the bond dimension variable in the current node to the sum 
+      of the bond dimensions associated with the child nodes
+
+      :param node: The node which will have its bond dimension set
+      :type node: ntreeNode
+      """
+      ind = 0
+      for i in range(node.size()):
+         ind += node.at(i).value
+      node.value = ind
+
+class NodeIncrementSetter(BondDimensionSetter):
+   def __init__(self, increment_value, combination="mean"):
+      """An implementation of the BondDimensionSetter abstract base class that set a given node
+      as an incremented value of a function of the children of the nodes
+
+      :param increment_value: The value to increment the node value by
+      :type increment_value: int
+      :param combination: How to combine the values of the child nodes to get the value to increment by, defaults to "mean"
+      :type combination: {"mean", "min", "max"}, optional
+      :raises RuntimeError: Raises an error if the combination variable is not in the allowed set of values
+      """
+      self.__increment_value = increment_value
+      if combination not in ["mean", "min", "max"]:
+         raise RuntimeError("Invalid combination rule for NodeIncrementSetter")
+      self.__child_combination = combination
+
+   def __call__(self, node):
+      """Set the value of the bond dimension variable in the current node to be
+      bond_dim = func(child_bond_dims) + inc
+
+      :param node: The node which will have its bond dimension set
+      :type node: ntreeNode
+      """
+      vars = []
+      for i in range(node.size()):
+         vars.append(node.at(i).value)
+      if self.__child_combination == "mean":
+         node.value = int(np.mean(vars)) + self.__increment_value
+      elif self.__child_combination == "min":
+         node.value = min(vars) + self.__increment_value
+      elif self.__child_combination == "max":
+         node.value = max(vars) + self.__increment_value
+      else:
+         raise RuntimeError("Invalid combination rule for NodeIncrementSetter")
---
+++ pyttn/ttns/topology/networkx_converter.py
@@ -0,0 +1,80 @@
+# This files is part of the pyTTN package.
+#(C) Copyright 2025 NPL Management Limited
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+import numpy as np
+import networkx as nx 
+
+from pyttn.ttnpp import ntree
+
+def convert_nx_to_subtree(tree, root, root_ind=0):
+   """A function for converting a networkx graph storing a tree structure into a subtree
+   of an ntree object with root at node root.   
+
+   :param tree: The networkx graph object representing the topology tree.  This 
+   :type tree: nx.Graph
+   :param root: The ntreeNode object that will be used as the root node to attach the current subtree to
+   :type root: ntreeNode
+   :param root_ind: The index in the tree object that should be connected to root, defaults to 0
+   :type root_ind: int, optional
+
+   :return: An array containing the index of the physical modes found at each leaf index
+   :rtype: list
+   """
+   root_skip = root.size()
+   node_dict = {root_ind: [root_skip]}
+   edge_counter = {}
+
+   node_inserted = False  
+   for edge in nx.dfs_edges(tree, source=root_ind):
+      #if this is the first edge in the dfs list then we need to insert both nodes
+      #so first insert left most node
+      if not node_inserted:
+         root.at([]).insert(edge[0])
+         node_inserted = True
+
+      if edge[0] not in edge_counter.keys():
+         edge_counter[edge[0]] = 0
+      else:
+         edge_counter[edge[0]] += 1
+
+      node_dict[edge[1]] = node_dict[edge[0]] + [edge_counter[edge[0]]]
+      root.at(node_dict[edge[0]]).insert(edge[1])
+   
+   return [root.at(leaf_inds).value for leaf_inds in root.leaf_indices()]
+
+
+def convert_nx_to_tree(tree, root_ind=0):
+   """A function for constructing an ntree object from a networkx object.  
+
+   :param tree: The networkx graph object representing the topology tree.  This 
+   :type tree: nx.Graph
+   :param root_ind: The index in the tree object that should be connected to root, defaults to 0
+   :type root_ind: int, optional
+   :return: An array containing the index of the physical modes found at each leaf index
+   :rtype: list
+   """
+   tree = ntree(str(tree.number_of_nodes()))
+
+   node_dict = {root_ind: []}
+   edge_counter = {}
+
+   for edge in nx.dfs_edges(tree, source=root_ind):
+
+      if edge[0] not in edge_counter.keys():
+         edge_counter[edge[0]] = 0
+      else:
+         edge_counter[edge[0]] += 1
+
+      node_dict[edge[1]] = node_dict[edge[0]] + [edge_counter[edge[0]]]
+      tree().at(node_dict[edge[0]]).insert(edge[1])
+
+   return tree, [leaf.value for leaf in tree.leaves()]
